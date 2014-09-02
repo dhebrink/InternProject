fbf19241e34142b3d46cd7b3a2f3176236eb1745
Daniel Hebrink <danielhebrink@gmail.com>
2014-09-01 22:55:19 -0500

    BONUS: Make new policy through webpage nearly there. View all policies/invoices working.

--- accounting/views.py
+++ accounting/views.py
@@ -4,6 +4,8 @@ from flask import render_template, request
 # Import things from Flask that we need.
 from accounting import app, db
 
+from datetime import datetime, date
+
 # Import our models
 from models import Contact, Invoice, Policy, Payment
 
@@ -44,3 +46,65 @@ def get_invoices(p_id):
     date_cursor = invoices[len(invoices)-1].cancel_date
     amt = pa.return_account_balance(date_cursor)
     return render_template('index.html', invoices=invoices, payments=payments, balance=amt)
+
+'''
+@app.route("/makepayment", methods=['GET', 'POST'])
+def make_payment():
+    if request.method == 'GET':
+        return render_template('payment.html')
+    else:
+        p_num = request.form['policy_num']
+        amt = request.form['amount']
+        try:
+            policy = Policy.query.filter(Policy.policy_number == p_num).one()
+            pa = PolicyAccounting(policy.id)
+            pa.make_payment(amount=amt)
+            return redirect(url_for('getinvoices', p_id=policy.id))
+        except:
+            return render_template('payment.html', policy_num=p_num, amount=amt)
+'''
+
+@app.route("/newpolicy", methods=['GET', 'POST'])
+def make_policy():
+    schedules = ["Annual", "Two-Pay", "Quarterly", "Monthly"]
+    i_contacts = Contact.query.filter(Contact.role == "Named Insured").all()
+    a_contacts = Contact.query.filter(Contact.role == "Agent").all()
+
+    if request.method == "POST":
+        #try:
+        p_num = request.form['policy_num']
+        eff_date = request.form['date']
+        bill_schedule = request.form['bill_schedule']
+        premium = request.form['premium']
+        policy = Policy(p_num, eff_date, premium)
+
+        policy.billing_schedule = bill_schedule
+
+        insured = request.form['insured']
+        if insured > 0: # If a 'Named Insured' was selected, tie to policy
+            policy.named_insured = insured
+        
+        agent = request.form['agent']
+        policy.agent = agent
+
+        db.session.add(policy)
+        db.session.commit()
+
+        # make PolicyAccounting object to initialize invoices
+        pa = PolicyAccounting(policy.id)
+
+        return redirect(url_for('getinvoices', p_id=policy.id))
+
+#        except:
+#           return render_template('create.html', schedules=schedules, insured_list=i_contacts, agent_list=a_contacts)
+
+    else:
+        return render_template('create.html', schedules=schedules, insured_list=i_contacts, agent_list=a_contacts)
+
+
+
+
+
+
+
+
